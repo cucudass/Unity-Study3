653354592f0ee4b80fceb5f8dd7aaab07ac416b6
조광래 <whrhkdfo12@naver.com>
2024-02-10 21:34:37 +0900

    240208_Playfab 기능

--- Photon Network/Assets/Scripts/Alarm.cs
+++ Photon Network/Assets/Scripts/Alarm.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum AlarmType { Alram, NickName }
+public enum AlarmType { Alarm, NickName }
 
 public class Alarm : MonoBehaviour
 {
